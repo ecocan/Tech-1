514928fd81e1cfb9823b91fc596a7a19d4a08fcc
gnctech52 <120405346+gnctech52@users.noreply.github.com>
2023-02-10 21:30:09 +0300

    Update test1.java
    gnc println eklendi.

--- src/main/java/day01/test1.java
+++ src/main/java/day01/test1.java
@@ -4,8 +4,8 @@ public class test1 {
 
     public static void main(String[] args) {
         System.out.println( "merhaba dünya");
-
         System.out.println( " deneme 2");
+         System.out.println( " deneme 3");
     }
 
 }
